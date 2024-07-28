ed25758151eb6517752c17d37d5a8247809f5b93
Samarth <srivastavasamarth94@gmail.com>
2024-07-29 00:08:25 +0530

    first commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,141 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel.js";
+import bcrypt from 'bcryptjs'
+
+export const register = async(req,res) => {
+    try {
+        const { fullname, email, phoneNumber, password, role } = req.body;
+         
+        if (!fullname || !email || !phoneNumber || !password || !role) {
+            return res.status(400).json({
+                message: "Missing Credntials",
+                success: false
+            });
+        };
+
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({
+                message:'User already exist with this email',
+                success:false,
+            })
+        }
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        await User.create({
+            fullname,
+            email,
+            phoneNumber,
+            password:hashedPassword,
+            role,
+        });
+        return res.status(201).json({
+            messagae:"Account successfully created",
+            success:true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+export const login = async(req,res)=>{
+    try {
+        const {email,password,role} = req.body;
+        if (!email || !password || !role) {
+            return res.status(400).json({
+                message: "Missing Credntials",
+                success: false
+            });
+        };
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                message:"Incorrect email or password",
+                success:false
+            })
+        };
+        if(role !== user.role){
+            return res.status(400).json({
+                message:"Account doesn't exist with current role",
+                success:false
+            })
+        };
+
+        const tokenData = {
+            userId:user._id
+        }
+        const token = await jwt.sign(tokenData,process.env.SECRET_KEY,{expiresIn:'1d'});
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+        }
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+            message: `Welcome back ${user.fullname}`,
+            user,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const logout = async(req,res)=>{
+    try {
+        return res.status(200).cookie("token","",{maxAge:0}).json({
+            message:"Logged out successfully",
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updateProfile = async(req,res)=>{
+    try {
+        const {fullname,email,phoneNumber,bio,skills} = req.body;
+        const file = req.file;
+        let skillsArray;
+        if(skills){
+            skillsArray = skills.split(",");
+        }
+        const userId = req.id;
+        let user = await User.findById(userId);
+        if(!user){
+            return res.status(400).json({
+                message:"User Not Found",
+                success:true
+            })
+        }
+
+        if(fullname) user.fullname = fullname
+        if(email) user.email = email
+        if(phoneNumber) user.phoneNumber = phoneNumber
+        if(bio) user.bio = bio;
+        if(skills) user.skills = skillsArray
+
+        await user.save();
+
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+        }
+
+        return res.status(200).json({
+            message:"Profile updated successfully",
+            user,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import dotenv from 'dotenv'
+import connectDB from './utils/database.js';
+import userRoute from './routes/userRoutes.js'
+dotenv.config({});
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http//localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+//api
+app.use("/api/user",userRoute)
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server started at port ${PORT}`);
+})
