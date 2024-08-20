cd57f963e7ee8aae2b9624fff7263cf8677b8e59
Samarth <srivastavasamarth94@gmail.com>
2024-08-20 13:07:33 +0530

    sixth commit

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { Popover,PopoverContent,PopoverTrigger } from '../ui/popover'
 import { Button } from '../ui/button'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { LogOut, User2 } from 'lucide-react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant.js'
+import { toast } from 'sonner'
+import { setAuthUser } from '@/redux/authSlice'
 
 
 const Navbar = () => {
   const {user} = useSelector(store=>store.auth)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutHandler = async () =>{
+    try {
+      const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+      if (res.data.success) {
+          dispatch(setAuthUser(null));
+          navigate("/");
+          toast.success(res.data.message);
+      }
+  } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+  }
+  }
   return (
     <div className=' bg-white'>
         <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -51,7 +71,7 @@ const Navbar = () => {
                     </div>
                     <div className=' flex w-fit items-center gap-2 cursor-pointer'>
                      <LogOut/> 
-                    <Button variant="link"><Link to='/login'>Logout</Link></Button>
+                    <Button onClick={logoutHandler} variant="link">Logout</Button>
                     </div>
                   </div>
                 </PopoverContent>
