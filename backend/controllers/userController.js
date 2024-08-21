ae77803585133eb550a9f45dfd2c6062015d53fd
Samarth <srivastavasamarth94@gmail.com>
2024-08-21 23:58:30 +0530

    cloudinary fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -104,7 +104,7 @@ export const updateProfile = async(req,res)=>{
         const file = req.file;
         //cloudinary
         const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content,{ format: "jpg", resource_type: "image" });
         let skillsArray;
         if(skills){
             skillsArray = skills.split(",");
