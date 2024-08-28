d14a1bab6d4f604f9c38061d63c4fb882b05f9e2
Samarth <srivastavasamarth94@gmail.com>
2024-08-28 23:08:43 +0530

    admin panel started

---
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
+import { Avatar, AvatarImage } from '../ui/avatar'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { Edit2, MoreHorizontal } from 'lucide-react'
+
+const CompaniesTable = () => {
+  return (
+    <div>
+        <Table>
+            <TableCaption>A list of your recent companies</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Logo</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Date</TableHead>
+                    <TableHead className='text-right'>Action</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableCell>
+                    <Avatar>
+                        <AvatarImage src="https://imgs.search.brave.com/dkZ18lo742IM19jY4my0sf1XytKeaf3SDRsQhH57drw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTI5/NzE1Mjg1NS92ZWN0/b3IvbG9nby13aXRo/LXRoZS1sZXR0ZXIt/Yy5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9XzZuSE9ReTNn/VjE2ZENIeGpUUUhM/b25JZWdWQU9YSm43/a012ZXJHdEZ3OD0"/>
+                    </Avatar>
+                </TableCell>
+                <TableCell>Microsoft</TableCell>
+                <TableCell>24.05.2024</TableCell>
+                <TableCell className='text-right cursor-pointer'>
+                    <Popover>
+                        <PopoverTrigger>
+                            <MoreHorizontal/>
+                        </PopoverTrigger>
+                        <PopoverContent className='w-32'>
+                            <div className='flex items-center gap-2 w-fit cursor-pointer'>
+                                <Edit2 className='w-4'/>
+                                <span>Edit</span>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
+                </TableCell>
+            </TableBody>
+        </Table>
+    </div>
+  )
+}
+
+export default CompaniesTable
