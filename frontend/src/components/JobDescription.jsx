8ccc8660aae413318d0fcaaf4dabdfd981eff394
Samarth <srivastavasamarth94@gmail.com>
2024-09-06 22:13:04 +0530

    eleventh commit

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -6,7 +6,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant'
 import { setSingleJob } from '@/redux/jobSlice'
-import { Toaster } from './ui/sonner'
 import { toast } from 'sonner'
 
 const JobDescription = () => {
